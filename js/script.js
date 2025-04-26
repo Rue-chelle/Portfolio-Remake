ebbb7cdf3649930ba8dd0570020403ee59207933
Alora <misshie21@gmail.com>
2025-04-26 10:17:53 +0200

    Typed script

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Typed.js Initialization
+var typed = new Typed("#typed", {
+  strings: ["Web Designer.", "Web Developer.", "Freelancer.", "Photographer."],
+  typeSpeed: 60,
+  backSpeed: 40,
+  loop: true
+});
